1c2cc739ec6657ba192bd3b61cd2de6502ebe471
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2021-10-24 23:59:02 -0700

    basic API test

--- node_backend/index.js
+++ node_backend/index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 
+// const swaggerUI = require("swagger-ui-express");
+// const swaggerJsDoc = require("swagger-jsdoc");
+
 app.use(cors({ origin: "*", credentials: true }));
 const user = require("./routes/userRoute");
 const connection = require("./db/connection");
@@ -12,19 +15,45 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({ origin: process.env.REACT_URL, credentials: true }));
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   next();
-// });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Library API",
+//       version: "1.0.0",
+//       description: "A simple Express Library API",
+//       termsOfService: "http://example.com/terms/",
+//       contact: {
+//         name: "API Support",
+//         url: "http://www.exmaple.com/support",
+//         email: "support@example.com",
+//       },
+//     },
+
+//     servers: [
+//       {
+//         url: "http://localhost:4001",
+//         description: "My API Documentation",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"],
+// };
 
 async function initializeApplication() {
   try {
@@ -44,5 +73,8 @@ initializeApplication()
     // logger.error(`Error in Initalizing Application  : ${error}`)
     console.log(error)
   );
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 module.exports = app;
---
+++ node_backend_del/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+app.use(cors({ origin: "*", credentials: true }));
+const user = require("./routes/userRoute");
+const connection = require("./db/connection");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors({ origin: process.env.REACT_URL, credentials: true }));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   next();
+// });
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+
+    servers: [
+      {
+        url: "http://localhost:4001",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+async function initializeApplication() {
+  try {
+    app.use(user);
+    await connection.createConnection();
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("App listening on port 8080");
+    });
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+}
+
+initializeApplication()
+  .then((response) => console.log("Server Running"))
+  .catch((error) =>
+    // logger.error(`Error in Initalizing Application  : ${error}`)
+    console.log(error)
+  );
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+module.exports = app;
