5c8728e03e655b4a61851c6381710baa817e0927
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-11-16 01:34:08 -0800

    removed unused files.

--- front_end/src/components/landingPage/landingPage.js
+++ front_end/src/components/landingPage/landingPage.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router";
-import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 class LandingPage extends React.Component {
   state = {};
+
   render() {
     return (
       <>
