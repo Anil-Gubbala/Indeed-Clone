f14ebafb3a41b524dbf7642981a1d9954cbaca64
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-11-25 00:04:38 -0800

    bug fix

--- front_end/src/routes/routes.js
+++ front_end/src/routes/routes.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import Register from "../components/auth/Register";
-import Login from "../components/auth/Login";
-import { Dashboard } from "../components/dashboard/Dashboard";
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Register from '../components/auth/Register';
+import Login from '../components/auth/Login';
+import { Dashboard } from '../components/dashboard/Dashboard';
 import companyHome from '../components/companyHome/companyHome';
 
 import NavBar from '../components/navbar/navbar';
@@ -11,7 +11,6 @@ class Routes extends Component {
   render() {
     return (
       <div>
-        
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/dashboard" component={Dashboard} />
