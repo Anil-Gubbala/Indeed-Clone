5ae8bebff0d54ab409f04fd175d34dcd257d0ba8
yashwanth6 <yashu654321@gmail.com>
2021-11-27 07:28:32 -0800

    Login, Register Using SQL and Dashboard Changes

--- node_backend/routes/userRoute.js
+++ node_backend/routes/userRoute.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const userService = require("../services/userService");
-const User=require("../db/schema/user")
+const User=require("../db/schema/user");
+var mysql = require('mysql');
+var constraints = require("../kafka/config");
 
+var sqlconnection = mysql.createPool({
+  host: constraints.DB.host,
+  user:constraints.DB.username,
+  password: constraints.DB.password,
+  port: constraints.DB.port,
+  database: constraints.DB.database
+ });
+
+ sqlconnection.getConnection((err) => {
+  if(err){
+      throw 'Error occured ' + err.message;
+  }
+  console.log("pool created");
+});
 // /**
 //  * @swagger
 //  *  tags:
@@ -31,7 +47,7 @@ const User=require("../db/schema/user")
 
 
 //post user - body details are emailid, password, accounttype
-router.post("/user", async (request, response) => {
+router.post("/use", async (request, response) => {
   console.log(request.body);
   try {
     const data = await userService.saveUserDetails(request);
@@ -46,13 +62,82 @@ router.post("/user", async (request, response) => {
   }
 });
 
+router.post("/user", async (request, response) => {
+  console.log(request.body);
+  
+    const {emailId,password,accountType} = request.body;
+    try{  
+        sqlconnection.query(`SELECT emailId FROM users WHERE emailId=?`,emailId
+        ,  function(error,results){
+            console.log(results);
+            if(results.length === 0){
+                console.log("New user");
+                sqlconnection.query(`Insert into users(emailId,password,accountType) values(?,?,?)`,[
+                    emailId,password,accountType],  function(error,results){
+                  if(error){
+                        response.writeHead(200, {
+                             'Content-Type': 'text-plain'
+                          });
+                         //response.send(error.code);
+                         response.send("failure");
+                     }else{
+                          response.writeHead(200,{
+                             'Content-Type': 'text/plain'
+                         });
+                         //res.end(JSON.stringify(results));
+                         response.end("success");
+                     }
+                 });
+          
+  }else{
+    //console.log("User already existed!");
+   response.send("failure");
+}
+});
+
+} catch (err) {
+    console.log(err);
+    const message = err.message
+      ? err.message
+      : "Error while Saving user Details";
+    const code = err.statusCode ? err.statusCode : 500;
+    return response.status(code).json({ message });
+  }
+});
+
+
+
 //Get user by id or by emailid, password and account type
-router.get("/user", async (request, response) => {
+router.get("/use", async (request, response) => {
   try {
     console.log(request.query);
     const data = await userService.getUserDetails(request);
     console.log(data);
     response.status(data.status).json(data.body);
+  } catch (err) {
+    console.log(err);
+    const message = err.message
+      ? err.message
+      : "Error while getting user Details";
+    const code = err.statusCode ? err.statusCode : 500;
+    return response.status(code).json({ message });
+  }
+});
+
+
+
+router.get("/user", async (request, response) => {
+  
+  const {emailId,password} = request.query;
+  try {
+    sqlconnection.query(`SELECT * FROM users WHERE emailId=? and password=?`,[emailId,password
+    ],  function(error,results){
+        if(results.length !== 0){
+            response.send(JSON.stringify(results));
+         }else{
+            response.send("failure");
+         }
+        });
   } catch (err) {
     console.log(err);
     const message = err.message
