810d85a8bb94bf09879671f094f67622d37f9a83
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2021-11-27 11:16:27 -0800

    messages

---
+++ front_end/src/components/whyjoinus/whyjoinus.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class WhyJoinUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <>No reason</>;
+  }
+}
+
+export default WhyJoinUs;
--- front_end/src/routes/routes.js
+++ front_end/src/routes/routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import companyHome from '../components/companyHome/companyHome';
+import Messages from '../components/messages/messages';
 
 import NavBar from '../components/navbar/navbar';
 
@@ -8,8 +9,9 @@ class Routes extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={NavBar} />
-        <Route exact path="/" component={companyHome} />
+        {/* <Route exact path="/" component={NavBar} />
+        <Route exact path="/" component={companyHome} /> */}
+        <Route exact path="/" component={Messages} />
       </div>
     );
   }
