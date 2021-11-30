9d7ba9f9d5373692c35247851e6c1d7d13d651e4
Reshma1KK <reshmakrishnakumar17@gmail.com>
2021-11-30 04:31:42 -0800

    Changed Front-end axios calls  and added input validations

--- front_end/src/components/EmployerLanding/ProfilePage.jsx
+++ front_end/src/components/EmployerLanding/ProfilePage.jsx
@@ -1,8 +1,9 @@
 import React,{useState,useRef,useEffect} from "react"
 import {Row,Col} from "react-bootstrap"
-import Axios from "axios"
+import { post,put } from '../../utils/serverCall';
 import "./ProfilePage.css"
 import NavBar from "./../navbar/employerNavBar"
+import { IoIosLock } from 'react-icons/io';
 
 const ProfilePage = () => {
 
@@ -10,15 +11,20 @@ const [employerData,setEmployerData] = useState([]);
 
 useEffect(() => {
   console.log("I am called");
-  Axios.post("http://localhost:8080/Profile",{
-    _id:"61a4739875c76feb200ca414",
-  }).then(response =>{
-    console.log(response.data.payload);
-    setEmployerData(response.data.payload);
+  post("/Profile",{_id:"61a4739875c76feb200ca414"})
+  .then(result =>{
+    console.log(result.payload);
+    setEmployerData(result.payload);
   })
 }, [])
 
-
+const getAcronym = (str1,str2) =>{
+  const newStr = str1 + " "+ str2;
+  var matches = newStr.match(/\b(\w)/g); // ['J','S','O','N']
+  var acronym = matches.join(''); // JSON
+  return acronym;
+  console.log("acronym",acronym);
+}
 
 
 
@@ -38,7 +44,13 @@ const [city,setCity] = useState("");
 const [state,setState] = useState("");
 const [zipCode,setZipCode] = useState("");
 
-
+const [firstNameErr,setFirstNameErr] = useState();
+const [lastNameErr,setLastNameErr] = useState("");
+const [roleErr,setRoleErr] = useState("");
+const [streetAddressErr,setStreetAddressErr] = useState("");
+const [cityErr,setCityErr] = useState("");
+const [stateErr,setStateErr] = useState("");
+const [zipCodeErr,setZipCodeErr] = useState("");
 
 
 function onClickOutSideContact(e) {
@@ -91,49 +103,113 @@ useEffect(() => {
 });
 
 const handleNameChange = () =>{
-    Axios.put("http://localhost:8080/EditCompanyName",{
-    _id:"61a4739875c76feb200ca414",
-    firstName:firstName,
-    lastName:lastName,
-    }).then((response) =>{
+  const isValid=nameValidation();
+  if(isValid){
+    put("/EditCompanyName",{_id:"61a4739875c76feb200ca414",firstName:firstName,lastName:lastName,})
+    .then((result) =>{
       console.log("Data Stored in Database");
     }).catch(err =>{
       console.log(err);
       console.log("Error saving in database")
     })
     window.location.reload();
+  }
 }
 
+
 const handleRoleChange = () =>{
-    Axios.put("http://localhost:8080/EditCompanyRole",{
-    _id:"61a4739875c76feb200ca414",
-    role:role,
-    }).then((response) =>{
+  const isValid=roleValidation();
+  if(isValid){
+    put("/EditCompanyRole",{_id:"61a4739875c76feb200ca414",role:role,})
+    .then((result) =>{
       console.log("Data Stored in Database");
     }).catch(err =>{
       console.log(err);
       console.log("Error saving in database")
     })
     window.location.reload();
+  }
 }
 
 const handleAddressChange = () =>{
-    Axios.put("http://localhost:8080/EditCompanyAddress",{
-    _id:"61a4739875c76feb200ca414",
-    streetAddress:streetAddress,
-    city:city,
-    state:state,
-    zipCode:zipCode,
-    }).then((response) =>{
+  const isValid=addressValidation();
+  if(isValid){
+    put("/EditCompanyAddress",{_id:"61a4739875c76feb200ca414",streetAddress:streetAddress,city:city,state:state,zipCode:zipCode})
+    .then((result) =>{
       console.log("Data Stored in Database");
     }).catch(err =>{
       console.log(err);
       console.log("Error saving in database")
     })
     window.location.reload();
+  }
 }
 
+const nameValidation = () =>{
+    setFirstNameErr("");
+    setLastNameErr("");
+    const firstNameErr="";
+    const lastNameErr="";
+    let isValid=true;
+  var nameExp = /^[a-zA-Z\s]*$/;
+  var regexName = new RegExp(nameExp);
+  if(firstName==="" || (firstName.search(regexName) == -1)){
+    isValid=false;
+    setFirstNameErr("Please enter a valid FirstName");
+  }
+  if(lastName==="" || (lastName.search(regexName) == -1)){
+    isValid=false;
+    setLastNameErr("Please enter a valid Lastname");
+  }
+  return isValid;
+}
 
+const roleValidation = () =>{
+    setRoleErr("");
+    const roleErr="";
+    let isValid=true;
+
+    if(role==""){
+      isValid=false;
+      setRoleErr("Please enter a valid role");
+    }
+    return isValid;
+}
+
+const addressValidation = () =>{
+  setStreetAddressErr("");
+  const streetAddressErr="";
+  setCityErr("");
+  const cityErr="";
+  setStateErr("");
+  const stateErr="";
+  setZipCodeErr("");
+  const zipCodeErr="";
+
+    let isValid=true;
+
+    if(streetAddress===""){
+      isValid=false;
+      setStreetAddressErr("Please enter a valid street");
+    }
+    var exp= /^[a-zA-Z][a-zA-Z ]*$/;
+    var regEx = new RegExp(exp);
+    if(city==="" || (city.search(regEx) == -1)){
+      isValid=false;
+      setCityErr("Please enter a valid city");
+    }
+    if(state==="" || (state.search(regEx) == -1)){
+      isValid=false;
+      setStateErr("Please enter a valid state");
+    }
+    var zipEx = /^\d+$/
+    var regZIP = new RegExp(zipEx);
+    if(zipCode==="" || (zipCode.search(regZIP) == -1) || (zipCode.length!=5)){
+      isValid=false;
+      setZipCodeErr("Please enter a valid zipcode");
+    }
+    return isValid;
+}
 
   return(
     <div className="body-profile">
@@ -143,20 +219,20 @@ const handleAddressChange = () =>{
     <Row>
     <Col>
     <div className="circle">
-    <p className="text"></p>
+    <h1 className="text">{getAcronym(employerData.firstName,employerData.lastName)}</h1>
     </div>
     </Col>
     <Col>
-    <h4 className="main-heading"><strong>{employerData.firstName} {employerData.lastName}</strong></h4>
+    <h2 className="main-heading"><strong>{employerData.firstName} {employerData.lastName}</strong></h2>
     </Col>
     </Row>
     </div>
       {editContactVisible ? (
         <div className="info-profile page-body shadow name-hidden">
-          <h4 className="heading-profile">Contact Information</h4>
+          <h6 className="heading-profile">Contact Information</h6>
         <div className="hidden-body">
-        <h6 className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</h6>
-        <h6 className="heading-profile">First Name<span style={{color:"red"}}>*</span></h6>
+        <p className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</p>
+        <p className="heading-profile">First Name<span style={{color:"red"}}>*</span></p>
           <input
           type="text"
           ref={inputRefContact}
@@ -168,7 +244,8 @@ const handleAddressChange = () =>{
             }
           }
           />
-          <h6 className="heading-profile">Last Name<span style={{color:"red"}}>*</span></h6>
+          <div className="error-msg" style={{color:"red"}}>{firstNameErr}</div>
+          <p className="heading-profile">Last Name<span style={{color:"red"}}>*</span></p>
           <input
           type="text"
           ref={inputRefContact}
@@ -180,6 +257,7 @@ const handleAddressChange = () =>{
             }
           }
           />
+          <div className="error-msg" style={{color:"red"}}>{lastNameErr}</div>
           <div className="save-button">
           <button type="button" className="btn btn-primary btn-md" onClick={handleNameChange}>Save</button>
           </div>
@@ -187,16 +265,16 @@ const handleAddressChange = () =>{
         </div>
               ) : (
           <div className="info-profile page-body shadow name">
-          <h4 className="heading-profile">Contact Information</h4>
+          <h6 className="heading-profile">Contact Information</h6>
           <div style={{textAlign:"left"}} className="hidden">
-          <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-90%",width:"30px"}} onClick={() => setEditContactVisible(true)}>
+          <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-99%",width:"3.5%"}} onClick={() => setEditContactVisible(true)}>
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path fill="black" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z">
               </path>
             </svg>
           </button>
           <div>
-          <h6>{employerData.firstName} {employerData.lastName}</h6>
+          <p>{employerData.firstName} {employerData.lastName}</p>
           </div>
         </div>
       </div>
@@ -205,10 +283,10 @@ const handleAddressChange = () =>{
       <div>
       {editRoleVisible ? (
         <div className="info-profile page-body shadow role-hidden">
-          <h4 className="heading-profile">Role</h4>
+          <h6 className="heading-profile">Role</h6>
         <div className="hidden-body-profile">
-          <h6 className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</h6>
-          <h6 className="heading-profile">Role in the Company<span style={{color:"red"}}>*</span></h6>
+          <p className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</p>
+          <p className="heading-profile">Role in the Company<span style={{color:"red"}}>*</span></p>
           <input
           type="text"
           ref={inputRefRole}
@@ -220,6 +298,7 @@ const handleAddressChange = () =>{
             }
           }
           />
+          <div className="error-msg" style={{color:"red"}}>{roleErr}</div>
           <div className="save-button">
           <button type="button" className="btn btn-primary btn-md" style={{margin:"30px"}} onClick={handleRoleChange}>Save</button>
           </div>
@@ -227,16 +306,16 @@ const handleAddressChange = () =>{
         </div>
         ) : (
           <div className="info-profile page-body shadow role">
-            <h4 className="heading-profile">Role</h4>
+            <h6 className="heading-profile">Role</h6>
           <div style={{textAlign:"left"}} className="hidden">
-          <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-90%",width:"30px"}} onClick={() => setEditRoleVisible(true)}>
+          <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-99%",width:"3.5%"}} onClick={() => setEditRoleVisible(true)}>
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path fill="black" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z">
               </path>
             </svg>
           </button>
           <div>
-        <h6>{employerData.role}</h6>
+        <p>{employerData.role}<span><IoIosLock /></span></p>
         </div>
         </div>
         </div>
@@ -245,10 +324,10 @@ const handleAddressChange = () =>{
         <div>
         {editAddressVisible ? (
           <div className="info-profile page-body shadow address-hidden">
-            <h4 className="heading-profile">Address</h4>
-            <div className="hidden-body">
-              <h6 className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</h6>
-              <h6 className="heading-profile">Address<span style={{color:"red"}}>*</span></h6>
+            <h6 className="heading-profile">Address</h6>
+            <div className="hidden-body-profile">
+              <p className="heading-profile"><span style={{color:"red"}}>*</span>Required Fields</p>
+              <p className="heading-profile">Address<span style={{color:"red"}}>*</span></p>
             <input
             type="text"
             ref={inputRefAddress}
@@ -260,7 +339,8 @@ const handleAddressChange = () =>{
               }
             }
             />
-            <h6 className="heading-profile">City<span style={{color:"red"}}>*</span></h6>
+            <div className="error-msg" style={{color:"red"}}>{streetAddressErr}</div>
+            <p className="heading-profile">City<span style={{color:"red"}}>*</span></p>
             <input
             type="text"
             ref={inputRefAddress}
@@ -272,7 +352,8 @@ const handleAddressChange = () =>{
               }
             }
             />
-            <h6 className="heading-profile">State<span style={{color:"red"}}>*</span></h6>
+            <div className="error-msg" style={{color:"red"}}>{cityErr}</div>
+            <p className="heading-profile">State<span style={{color:"red"}}>*</span></p>
             <input
             type="text"
             ref={inputRefAddress}
@@ -284,7 +365,8 @@ const handleAddressChange = () =>{
               }
             }
             />
-            <h6 className="heading-profile">ZIP Code<span style={{color:"red"}}>*</span></h6>
+            <div className="error-msg" style={{color:"red"}}>{stateErr}</div>
+            <p className="heading-profile">ZIP Code<span style={{color:"red"}}>*</span></p>
             <input
             type="text"
             ref={inputRefAddress}
@@ -296,6 +378,7 @@ const handleAddressChange = () =>{
               }
             }
             />
+            <div className="error-msg" style={{color:"red"}}>{zipCodeErr}</div>
             <div className="save-button">
             <button type="button" className="btn btn-primary btn-md" style={{margin:"30px",borderRadius:"10%"}} onClick={handleAddressChange}>Save</button>
             </div>
@@ -303,25 +386,25 @@ const handleAddressChange = () =>{
           </div>
           ) : (
             <div className="info-profile page-body shadow address">
-            <h4 className="heading-profile">Address</h4>
+            <h6 className="heading-profile">Address</h6>
             <div style={{textAlign:"left"}} className="hidden">
-            <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-90%",width:"30px"}} onClick={() => setEditAddressVisible(true)}>
+            <button type="button" style={{backgroundColor:"white",border:"none",justifyContent:"right",position:"relative",top:"50%",right:"-99%",width:"3.5%"}} onClick={() => setEditAddressVisible(true)}>
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path fill="black" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z">
                 </path>
               </svg>
             </button>
             <div>
-            <h6>{employerData.streetAddress}</h6>
+            <p>{employerData.streetAddress}</p>
             </div>
             <div>
-            <h6>{employerData.city}</h6>
+            <p>{employerData.city}</p>
             </div>
             <div>
-            <h6>{employerData.state}</h6>
+            <p>{employerData.state}</p>
             </div>
             <div>
-            <h6>{employerData.zipCode}</h6>
+            <p>{employerData.zipCode}</p>
             </div>
           </div>
         </div>
