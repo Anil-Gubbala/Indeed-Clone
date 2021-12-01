b7c5d13cc018c6803b3b1c610ee681d1df796cb0
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-11-30 19:35:02 -0800

    added local storage.

--- front_end/src/utils/checkLogin.js
+++ front_end/src/utils/checkLogin.js
@@ -7,3 +7,12 @@ export const isEmployer = () =>
   JSON.parse(localStorage.getItem(STORAGE.accountType)) === 'customer';
 export const isCustomer = () => JSON.parse(localStorage.getItem(STORAGE.accountType)) === 'admin';
 export const getEmailId = () => JSON.parse(localStorage.getItem(STORAGE.emailId));
+
+
+export const setLogin = (info) => {
+  localStorage.setItem(STORAGE.isSignedIn,true);
+  localStorage.setItem(STORAGE.accountType,info.user.accountType);
+  localStorage.setItem(STORAGE.emailId,info.user.emailId);
+  localStorage.setItem(STORAGE.token,info.token);
+  localStorage.setItem(STORAGE.companyId, info.user.companyId);
+}
--- front_end/src/utils/consts.js
+++ front_end/src/utils/consts.js
@@ -2,4 +2,12 @@ const config = {
   SERVERURL: 'http://localhost:8080',
 };
 
+export const STORAGE = {
+  isSignedIn: 'isSignedIn',
+  accountType: 'accountType',
+  emailId: 'emailId',
+  token: 'token',
+  companyId: 'companyId'
+};
+
 export default config;
